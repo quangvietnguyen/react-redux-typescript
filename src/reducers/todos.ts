492efa871ea7451bf54f4f61470499c970bfa8c3
nguye374 <nguye374@sheridancollege.ca>
2022-07-10 17:51:31 -0400

    finish

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,10 +1,21 @@
-import { FetchTodosAction, Todo } from '../actions';
-import { ActionTypes } from '../actions/types';
+import {
+  //   FetchTodosAction,
+  //   DeleteTodoAction,
+  Action,
+  Todo,
+  ActionTypes,
+} from '../actions';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (
+  state: Todo[] = [],
+  //   action: FetchTodosAction | DeleteTodoAction
+  action: Action
+) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
